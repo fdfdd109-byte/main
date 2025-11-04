7bd828c39a70ce8ff82747d2442dd6674b4dc74d
fdfdd109-byte <fdfdd109@gmail.com>
2025-11-04 21:25:13 +0300

    Add files via upload

---
+++ shooter_game.py
@@ -0,0 +1,46 @@
+#Создай собственный Шутер!
+
+from pygame import *
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+	    self.image = transform.scale(image.load(player_image), (65, 65))    
+	    self.speed = player_speed
+	    self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y 
+    def reset(self):
+    window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_ptessed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect < win_width - 80:
+    def fire(self):
+        pass
+
+a = Player(GameSprite, 65, 65, 5)
+
+win_width = 700
+win_height = 500
+
+window = display.set_mode((700, 500))
+display.set_caption('лябуба')
+background = transform.scale(image.load('galaxy.jpg'), (700, 500))
+mixer.init()
+mixer.music.load('space.ogg')
+mixer.music.play()
+clock = time.Clock()
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False 
+    window.blit(background, (0, 0))
+
+
+    display.update()
+clock.tick(60)
+
